af50b2265db1d44f2d4919ae4376daaf0fbffc71
Dano Colombo <danocolombo@gmail.com>
2021-12-12 14:26:43 -0500

    email a test message

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,34 @@
 const functions = require("firebase-functions");
-// const config = functions.config();
+const config = functions.config();
 const admin = require("firebase-admin");
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 const cors = require("cors")({origin: true});
 
 admin.initializeApp();
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: config.user.email,
+    pass: config.user.password,
+  },
+});
 
-exports.sendAnMail = functions.https.onRequest((req, res) => {
+exports.sendAnEmail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
-    res.send("Message sent successfully.");
+    const mailOptions = {
+      from: "DColombo.com Web Site",
+      to: "danocolombo@gmail.com",
+      subject: "Just a Test",
+      html: `
+        <p style="font-size: 16px;">From: Dano</p>
+        `,
+    };
+    try {
+      transporter.sendMail(mailOptions);
+      res.send("Message sent successfully.");
+    } catch (error) {
+      res.send(error);
+    }
   });
 });
